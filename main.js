5550100226aa1633fd8e8d46f9469c373074394f
Ryan Frazier <rfrazier@jacksonfurnind.com>
2019-05-16 11:01:11 -0400

    completed tutorial for shuffling a deck of cards

--- main.js
+++ main.js
@@ -4,13 +4,63 @@ $(document).ready(function () {
     var vm = new Vue({
         el: '#app',
         data: {
+            ranks: ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'],
+            suits: ['♥', '♦', '♠', '♣'],
+            cards: [],
+            suitColor: {
+                '♠': 'black',
+                '♣': 'black',
+                '♦': 'red',
+                '♥': 'red',
+            },
+            shuffleSpeed: 'shuffleMedium',
+            shuffleTypes: ['Slow', 'Medium', 'Fast'],
+            isDeckShuffled: false,
+            shuffleCount: 0
+        },
+        created() {
+            this.displayInitialDeck();
+        },
+        methods: {
+            displayInitialDeck() {
+                let id = 1;
+                this.cards = [];
 
-        }
+                for (let s = 0; s < this.suits.length; s++) {
+                    for (let r = 0; r < this.ranks.length; r++) {
+                        let card = {
+                            id: id,
+                            rank: this.ranks[r],
+                            suit: this.suits[s]
+                        }
+                        this.cards.push(card)
+                        id++
+                    }
+                }
+                this.isDeckShuffled = false;
+                this.shuffleCount = 0;
+            },
+            shuffleDeck() {
+                // Example of Fisher-Yates algorithom for random order
+                for (let i = this.cards.length - 1; i > 0; i--) {
+                    let randomIndex = Math.floor(Math.random() * i);
+
+                    let temp = this.cards[i];
+                    // this.cards[i] = this.cards[randomIndex];
+                    Vue.set(this.cards, i, this.cards[randomIndex]);
+                    // this.cards[randomIndex] = temp;
+                    Vue.set(this.cards, randomIndex, temp);
+                }
+                this.isDeckShuffled = true;
+                this.shuffleCount = this.shuffleCount + 1;
+            },
+        },
+
+
     })
 
 
 
 
-
-
 });
+
